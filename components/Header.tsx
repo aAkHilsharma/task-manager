72b5c346f89695b1cef241c14d5df52fd5187ef1
aAkHilsharma <akhilsharma6721@gmail.com>
2023-09-07 19:44:04 +0530

    Add header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+"use client";
+
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="p-5 bg-gray-500/10 rounded-b-2xl">
+        <div className="absolute top-0 left-0 w-full h-96 bg-gradient-to-br from-pink-400 to-[#0055D1] rounded-md filter blur-3xl opacity-50 -z-50" />
+        <div className="flex items-center space-x-5 flex-1 justify-between w-full">
+          <div className="text-2xl font-bold text-[#0055d1]">Trello</div>
+          <div className="">
+            <form className="flex items-center space-x-5 rounded-md p-2 shadow-md flex-1 md:flex-initial bg-white">
+              <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
+              <input
+                type="text"
+                placeholder="Search"
+                className="flex-1 outline-none p-2"
+              />
+              <button type="submit" hidden>
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
