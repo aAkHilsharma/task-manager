bc72de0d500059ea01d5087e97d97cb5d117fff0
aAkHilsharma <akhilsharma6721@gmail.com>
2023-09-08 15:01:31 +0530

    add crud for todo

--- store/BoardStore.ts
+++ store/BoardStore.ts
@@ -8,6 +8,24 @@ interface BoardState {
   setBoardState: (board: Board) => void;
   updateTodoInDb: (todo: Todo, columnId: TypedColumn) => void;
   deleteTask: (taskIndex: number, todoId: Todo, id: TypedColumn) => void;
+  addTask: (title: string, columnId: TypedColumn, description: string) => void;
+  $id: string;
+  setId: (id: string) => void;
+  prevTaskType: TypedColumn;
+  setPrevTaskType: (columnId: TypedColumn) => void;
+  updateTask: (
+    $id: string,
+    title: string,
+    columnId: TypedColumn,
+    prevTaskType: TypedColumn,
+    description: string
+  ) => void;
+  newTitleInput: string;
+  newDescriptionInput: string;
+  setNewTitleInput: (input: string) => void;
+  setNewDescriptionInput: (input: string) => void;
+  newTaskType: TypedColumn;
+  setNewTaskType: (columnId: TypedColumn) => void;
 }
 
 export const useBoardStore = create<BoardState>((set, get) => ({
@@ -20,6 +38,15 @@ export const useBoardStore = create<BoardState>((set, get) => ({
     set({ board });
   },
   setBoardState: (board) => set({ board }),
+
+  newTitleInput: "",
+  newDescriptionInput: "",
+
+  setNewTitleInput: (input: string) => set({ newTitleInput: input }),
+  setNewDescriptionInput: (input) => set({ newDescriptionInput: input }),
+
+  $id: "",
+  setId: (id: string) => set({ $id: id }),
 
   updateTodoInDb: async (todo, columnId) => {
     await databases.updateDocument(
@@ -47,4 +74,111 @@ export const useBoardStore = create<BoardState>((set, get) => ({
       todo.$id
     );
   },
+
+  addTask: async (
+    title: string,
+    columnId: TypedColumn,
+    description: string
+  ) => {
+    const { $id } = await databases.createDocument(
+      process.env.NEXT_PUBLIC_DATABASE_ID!,
+      process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!,
+      ID.unique(),
+      {
+        title,
+        description,
+        status: columnId,
+      }
+    );
+
+    set({ newTitleInput: "" });
+    set({ newDescriptionInput: "" });
+
+    set((state) => {
+      const newColumns = new Map(state.board.columns);
+
+      const newTodo: Todo = {
+        $id,
+        $createdAt: new Date().toISOString(),
+        title: title,
+        description: description,
+        status: columnId,
+      };
+
+      const column = newColumns.get(columnId);
+
+      if (!column) {
+        newColumns.set(columnId, {
+          id: columnId,
+          todos: [newTodo],
+        });
+      } else {
+        newColumns.get(columnId)?.todos.push(newTodo);
+      }
+
+      return {
+        board: {
+          columns: newColumns,
+        },
+      };
+    });
+  },
+
+  updateTask: async (
+    $id: string,
+    title: string,
+    columnId: TypedColumn,
+    prevTaskType: TypedColumn,
+    description: string
+  ) => {
+    const response = await databases.updateDocument(
+      process.env.NEXT_PUBLIC_DATABASE_ID!,
+      process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!,
+      $id,
+      {
+        title,
+        status: columnId,
+        description,
+      }
+    );
+    set({ newTitleInput: "" });
+    set({ newDescriptionInput: "" });
+    set({ $id: "" });
+    set({ newTaskType: "todo" });
+
+    const updatedTodo: Todo = {
+      title: response.title,
+      description: response.description,
+      status: response.status,
+      $id: response.$id,
+      $createdAt: response.$createdAt,
+    };
+
+    set((state) => {
+      const newColumns = new Map(state.board.columns);
+      const removedTodos = newColumns.get(prevTaskType)?.todos;
+      const indexToRemove = removedTodos?.findIndex((todo) => todo.$id === $id);
+
+      if (indexToRemove !== undefined && indexToRemove !== -1 && removedTodos) {
+        removedTodos.splice(indexToRemove, 1);
+      }
+
+      const newTodos = newColumns.get(columnId)?.todos;
+
+      if (newTodos) {
+        newTodos.push(updatedTodo);
+      }
+
+      return {
+        board: {
+          columns: newColumns,
+        },
+      };
+    });
+  },
+
+  newTaskType: "todo",
+  setNewTaskType: (ColumnId: TypedColumn) => set({ newTaskType: ColumnId }),
+  prevTaskType: "todo",
+  setPrevTaskType: (ColumnId: TypedColumn) => set({ prevTaskType: ColumnId }),
 }));
