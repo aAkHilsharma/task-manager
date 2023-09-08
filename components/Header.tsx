3abb0a89fe7b7fdacb00961da168cf2e8587d92a
aAkHilsharma <akhilsharma6721@gmail.com>
2023-09-08 12:33:32 +0530

    minor changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 
 function Header() {
   return (
-    <header>
+    <header className="mb-20">
       <div className="p-5 bg-gray-500/10 rounded-b-2xl">
         <div className="absolute top-0 left-0 w-full h-96 bg-gradient-to-br from-pink-400 to-[#0055D1] rounded-md filter blur-3xl opacity-50 -z-50" />
         <div className="flex items-center space-x-5 flex-1 justify-between w-full">
